c0572f77c7301c7a6d97c7acb1fd998c5cc23d93
qinwankang <273509825@qq.com>
2018-06-30 17:10:13 +0800

    Merge branch 'master' of https://github.com/qinwankang/springboot-demo
    # Conflicts:
    #	src/main/java/com/jk/dao/AticleDao.java

--- src/main/java/com/jk/dao/AticleDao.java
+++ src/main/java/com/jk/dao/AticleDao.java
@@ -7,11 +7,8 @@ import java.math.BigDecimal;
 import java.util.List;
 
 /**
- * Created by Administrator on 2018/4/19 0019. 这个是分支
- * --改一下，，，这个是实际上主干--现在是1646
+ * Created by Administrator on 2018/4/19 0019. 这个是主干
  */
 public interface AticleDao extends JpaRepository<Aticle,BigDecimal> {
     List<Aticle> findAll();
-    List<Aticle> findAll1();
-    List<Aticle> findAll2();
 }
